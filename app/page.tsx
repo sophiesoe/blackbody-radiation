f561d84c280e7b931d60b94c1eefb637cd49bb14
sophiesoe <devsophiesoe@gmail.com>
2025-11-02 00:25:41 +0530

    add: particles bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,24 @@ import dynamic from "next/dynamic";
 const BlackbodyApplet = dynamic(() => import("../components/BlackbodyApplet"), {
   ssr: false,
 });
+import ParticlesBackground from "../components/ParticlesBackground";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-between items-center pt-10 overflow-hidden">
+    <main className="h-screen flex flex-col justify-center items-center overflow-hidden my-auto">
+      <ParticlesBackground />
       <BlackbodyApplet />
-      <footer className="md:flex w-full justify-between items-center p-2 text-xs md:text-sm text-center md:text-left">
-        <div>All rights reserves &copy; {new Date().getFullYear()}, </div>
-        <div>
-          <span className="text-[#f9f871]">Brisa, Varsheetha, Thin </span>
-          <span>
-            {"[ "}CSE P-batch{" ]"}
-          </span>
-        </div>
-      </footer>
+      <div className="fixed bottom-0 w-full">
+        <footer className="md:flex w-full justify-between items-center p-2 text-xs md:text-sm text-center md:text-left">
+          <div>All rights reserves &copy; {new Date().getFullYear()}, </div>
+          <div>
+            <span className="text-[#f9f871]">Brisa, Varsheetha, Thin </span>
+            <span>
+              {"[ "}CSE P-batch{" ]"}
+            </span>
+          </div>
+        </footer>
+      </div>
     </main>
   );
 }
